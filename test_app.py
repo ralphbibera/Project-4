5dfc9945761c23bd032eb698bb6fe452f3c477c4
ralphbibera <98459325+ralphbibera@users.noreply.github.com>
2022-11-07 17:43:37 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -11,12 +11,11 @@ def test_1():
 
 
 def test_2():
-      orig = "Manila"
       set_keyboard_input(["Manila","Q"])
 
       orig, dest = enterInput()
 
-      output = checkInput()
+      output = checkInput(orig, dest)
       
       assert output == "EXIT"
       
