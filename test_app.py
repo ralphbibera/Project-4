64e89052ef40696d87ab7f054481a72974c8119f
ralphbibera <98459325+ralphbibera@users.noreply.github.com>
2022-11-07 15:35:18 +0800

    Update

--- test_app.py
+++ test_app.py
@@ -1,13 +1,21 @@
-from app import enterInput
+from trial import trial_function
 from tud_test_base import set_keyboard_input, get_display_output
+
+# def test_1():
+#       set_keyboard_input(["Manila","Q"])
+
+#       enterInput()
+
+#       output = get_display_output()
+
+#       assert output == ["Starting Location: ","Destination: "]
+
 
 def test_1():
       set_keyboard_input(["Manila","Q"])
 
-      enterInput()
+      trial_function()
 
       output = get_display_output()
-
       assert output == ["Starting Location: ","Destination: "]
-
 
---
+++ trial.py
@@ -0,0 +1,9 @@
+def trial_function():
+    orig = input("Starting Location: ")
+    dest = input("Destination: ")
+
+    return orig, dest
+
+
+if __name__ == "__main__":
+    trial_function()
